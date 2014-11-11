f2abfecc7a5345ecdc458bb39f673fd5e0c4d173
sean <codemyksy@hotmail.com>
2014-11-11 17:03:09 -0500

    +cas consumer

---
+++ project-team05/src/main/java/consumer/BioConsumer.java
@@ -0,0 +1,15 @@
+package consumer;
+
+import org.apache.uima.cas.CAS;
+import org.apache.uima.collection.CasConsumer_ImplBase;
+import org.apache.uima.resource.ResourceProcessException;
+
+public class BioConsumer extends CasConsumer_ImplBase {
+
+	@Override
+	public void processCas(CAS aCAS) throws ResourceProcessException {
+		// TODO Auto-generated method stub
+
+	}
+
+}

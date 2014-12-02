de43082430d297fbfe9fd5bc81646ef458265de9
freddiezhang <fredzhang0509@gmail.com>
2014-12-01 23:23:11 -0500

    all evaluators

--- project-team05/src/main/java/edu/cmu/lti/f14/team05/Evaluator.java
+++ project-team05/src/main/java/edu/cmu/lti/f14/team05/Evaluator.java
@@ -12,6 +12,7 @@ import java.util.Set;
 import json.JsonCollectionReaderHelper;
 import json.gson.Question;
 import json.gson.Snippet;
+import json.gson.TestListQuestion;
 import json.gson.TestQuestion;
 import json.gson.Triple;
 
@@ -23,7 +24,7 @@ public class Evaluator {
 	Map<String, List<String>> goldConcepts;
 	Map<String, List<Triple>> goldTriples;
 	Map<String, List<Snippet>> goldSnippets;
-	Map<String, String> goldAnswers;
+	Map<String, List<List<String>>> goldAnswers;
 	FileWriter fout;
 	FileWriter report;
 	String evalResult = new String();
@@ -34,22 +35,30 @@ public class Evaluator {
 		goldConcepts = new HashMap<String, List<String>>();
 		goldTriples = new HashMap<String, List<Triple>>();
 		goldSnippets = new HashMap<String, List<Snippet>>();
-		goldAnswers = new HashMap<String, String>();
+		goldAnswers = new HashMap<String, List<List<String>>>();
 		for (TestQuestion question: goldStandards){
-			goldDocuments.put(question.getId(), question.getDocuments());
-			goldConcepts.put(question.getId(), question.getConcepts());
-			goldTriples.put(question.getId(), question.getTriples());
-			goldSnippets.put(question.getId(), question.getSnippets());
-			goldAnswers.put(question.getId(), question.getIdealAnswer());
+				if (question instanceof TestListQuestion ){
+				goldDocuments.put(question.getId(), question.getDocuments());
+				goldConcepts.put(question.getId(), question.getConcepts());
+				goldTriples.put(question.getId(), question.getTriples());
+				goldSnippets.put(question.getId(), question.getSnippets());
+				goldAnswers.put(question.getId(), ((TestListQuestion) question).getExactAnswer());
+			}
 		}
 	}
 	public void outputEvaluate(List<ExactAnswer> testAnswer){
+		try {
+			fout = new FileWriter("Evaluation Result.txt");
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 		List<EvaluationResult> conceptsEval = new ArrayList<EvaluationResult>();
 	    List<EvaluationResult> documentsEval = new ArrayList<EvaluationResult>();
 	    List<EvaluationResult> triplesEval = new ArrayList<EvaluationResult>();
 	    List<EvaluationResult> snippetsEval = new ArrayList<EvaluationResult>();
 	    List<EvaluationResult> answersEval = new ArrayList<EvaluationResult>();
-		for (Question q: testAnswer){		
+		for (TestListQuestion q: testAnswer){		
 			evalResult +="Question: "+ q.getBody() + "\n";
 			evalResult +="Question Type: "+ q.getType() + "\n";
 			boolean exist = false;
@@ -63,11 +72,32 @@ public class Evaluator {
 				documentsEval.add(evalDocuments(goldDocuments.get(q.getId()),q.getDocuments()));
 				triplesEval.add(evalTriples(goldTriples.get(q.getId()),q.getTriples()));
 				snippetsEval.add(evalSnippets(goldSnippets.get(q.getId()),q.getSnippets()));
-				//answer evaluate
+				answersEval.add(evalAnswers(goldAnswers.get(q.getId()),q.getExactAnswer()));
 			}
 		}
+		double[] conceptsMeans = solveMeanMetrics(conceptsEval);
+		double[] documentsMeans = solveMeanMetrics(documentsEval);
+		double[] triplesMeans = solveMeanMetrics(triplesEval);
+		double[] snippetsMeans = solveMeanMetrics(snippetsEval);
+		double[] answersMeans = solveMeanMetrics(answersEval);
+		evalResult += "Mean Evaluations:\nItem, Precision, Recall, F-measure, MAP, GMAP\n";
+		String conceptLine = String.format("Concepts  %.3f %.3f %.3f %.3f %.3f \n",conceptsMeans[0],
+				conceptsMeans[1],conceptsMeans[2],conceptsMeans[3],conceptsMeans[4]);
+		String documentLine = String.format("Documents  %.3f %.3f %.3f %.3f %.3f \n",documentsMeans[0],
+				documentsMeans[1],documentsMeans[2],documentsMeans[3],documentsMeans[4]);
+		String tripleLine = String.format("Triples  %.3f %.3f %.3f %.3f %.3f \n",triplesMeans[0] 
+				,triplesMeans[1],triplesMeans[2],triplesMeans[3],triplesMeans[4]);
+		String snippetLine = String.format("Snippets  %.3f %.3f %.3f %.3f %.3f \n",snippetsMeans[0] 
+				,snippetsMeans[1],snippetsMeans[2],snippetsMeans[3],snippetsMeans[4]);
+		evalResult += conceptLine;
+		evalResult += documentLine;
+		evalResult += tripleLine;
+		evalResult += snippetLine;
+		evalResult += "Answer, Precision, Recall, F-measure\n";
+		String answerLine = String.format("Concepts  %.3f %.3f %.3f\n",answersMeans[0]
+				,answersMeans[1],answersMeans[2]);
+		evalResult += answerLine;
 		try {
-			fout = new FileWriter("Evaluation Result.txt");
 			fout.write(evalResult);
 			fout.close();
 		} catch (IOException e) {
@@ -139,11 +169,24 @@ public class Evaluator {
 	    evalResult += ap + "\n";
 		return new EvaluationResult(precision,recall,fmeasure,ap);
 	}
+	public EvaluationResult evalAnswers (List<List<String>> gold, List<List<String>> test){
+		evalResult += "ExactAnswer:\n";
+		double precision = solveAnswerPrecision(gold,test);
+		double recall = solveAnswerRecall(gold,test);
+		double fmeasure = solveFMeasure(precision, recall);
+		evalResult += "Precision of exact answer is:\n";
+		evalResult += precision + "\n";
+		evalResult += "Recall of exact answer is:\n";
+		evalResult += recall + "\n";
+		evalResult += "F score of exact answer is:\n";
+	    evalResult += fmeasure + "\n";
+		return new EvaluationResult(precision, recall , fmeasure , 0.0);
+	}
 	private <T> List<T> emptyListIfNull(List<T> list) {
 	    if (list == null)
 	      return new ArrayList<T>();
 	    return list;
-	  }
+	}
 	private <T> double solvePrecision(List<T> gold, List<T> test){
 		gold = emptyListIfNull(gold);
 		test = emptyListIfNull(test);
@@ -253,5 +296,74 @@ public class Evaluator {
 	    }
 	    return ap / (count + Math.pow(10, -10));
 	}
-	
+	private double solveAnswerPrecision (List<List<String>> gold, List<List<String>> test){
+		double result = 0.0;
+		int testCount = 0;
+		int matches = 0;
+		List<String> goldList = new ArrayList<String>();
+		List<String> testList = new ArrayList<String>();
+		for (List<String> g : gold){
+			for (String gg : g){
+				goldList.add(gg);
+			}
+		}
+		for (List<String> t : test){
+			for (String tt : t){
+				testList.add(tt);
+				testCount++;
+			}
+		}
+		for (String t : testList){
+			for (String m : goldList){
+				if (m.equals(t))
+					matches++;
+			}
+		}
+		result = (double) matches / (double) testCount;
+		return result;
+	}
+	private double solveAnswerRecall (List<List<String>> gold, List<List<String>> test){
+		double result = 0.0;
+		int goldCount = 0;
+		int matches = 0;
+		List<String> goldList = new ArrayList<String>();
+		List<String> testList = new ArrayList<String>();
+		for (List<String> g : gold){
+			for (String gg : g){
+				goldList.add(gg);
+				goldCount++;
+			}
+		}
+		for (List<String> t : test){
+			for (String tt : t){
+				testList.add(tt);
+			}
+		}
+		for (String t : testList){
+			for (String m : goldList){
+				if (m.equals(t))
+					matches++;
+			}
+		}
+		result = (double) matches / (double) goldCount;
+		return result;
+	}
+	private double[] solveMeanMetrics(List<EvaluationResult> results) {
+	    double epsilon = 0.001;
+	    double meanPrec = 0;
+	    double meanRec = 0;
+	    double meanFmeas = 0;
+	    double MAP = 0;
+	    double GMAP = 1;
+	    int numOfQuery = results.size();
+	    for (EvaluationResult eval : results) {
+	      meanPrec += eval.getPrecision();
+	      meanRec += eval.getRecall();
+	      meanFmeas += eval.getfMeasure();
+	      MAP += eval.getAvgPrecison();
+	      GMAP *= (eval.getAvgPrecison() + epsilon);
+	    }
+	    return new double[] { meanPrec / numOfQuery, meanRec / numOfQuery, meanFmeas / numOfQuery, 
+	            MAP / numOfQuery, Math.pow(GMAP, 1.0 / numOfQuery) };
+	  }
 }
